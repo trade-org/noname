35e5069c70a4b89ea8316ba343b9b0014d6e0b3d
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-30 18:48:06 +0100

    category..

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainSection from "../components/MainSection";
 import Navbar from "../components/Navbar";
+import Category from "../components/Category";
 
 const Homepage = () => {
   return (
     <div>
       <Navbar />
       <MainSection />
+      <Category />
     </div>
   );
 };
