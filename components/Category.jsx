53d9fb3c152e0e179385c859253924529de9540e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-30 07:04:42 +0100

    ..

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,32 +1,44 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 import React from "react";
 
 const Category = () => {
   return (
-    <div>
-      <Tabs isFitted variant="enclosed">
-        <TabList mb="1em">
-          <Tab>All Category</Tab>
-          <Tab>Accessories</Tab>
-          <Tab>Fashion</Tab>
-          <Tab>Cars</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </div>
+    <>
+      <Center>Category</Center>
+      <Box mt={"50px"} px={"100px"}>
+        <Tabs isFitted variant="enclosed">
+          <TabList mb="1em">
+            <Tab>All Category</Tab>
+            <Tab>Accessories</Tab>
+            <Tab>Fashion</Tab>
+            <Tab>Cars</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </>
   );
 };
 
