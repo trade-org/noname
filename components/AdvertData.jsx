c747eecbf81b552b602008f7934fb8e2adf56362
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-12-03 00:13:14 +0100

    ads data

---
+++ components/AdvertData.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const AdvertData = [
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+  {
+    name: "Waist Chain",
+    price: "₦ 5,500",
+    imagelink:
+      "https://pictures-nigeria.jijistatic.com/107747445_NzA0LTk1NS1kZTI0ODYxODM1.webp",
+  },
+];
+
+export default AdvertData;
